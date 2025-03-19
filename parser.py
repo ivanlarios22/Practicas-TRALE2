cfa8af896e9f86927ec03b61f433e05d61e19d13
alecksandr <sansepiol26@gmail.com>
2025-03-18 20:54:56 -0600

    New parser validation

--- parser.py
+++ parser.py
@@ -5,8 +5,18 @@
 # digit -> 0 | 1 | 2 | ... | 9
 
 
+
 # Lexer: A simple class to pass from the stream of chars to tokens
 class Lexer:
+    # A few tokens, at the end tokes are pairs of tuples
+    EOF = ('EOF', 'EOF')
+    PLUS = ('PLUS', '+')
+    SUB = ('SUB', '-')
+    MUL = ('MUL', '*')
+    DIV = ('DIV', '/')
+    LPAREN = ('LPAREN', '(')
+    RPAREN = ('RPAREN', ')')
+    
     def __init__(self, input_string):
         self.input_string = input_string 
         self.tokens = []        # Will contain all the tokens
@@ -20,15 +30,25 @@ class Lexer:
         """
         
         s = self.input_string
+        if s == "":
+            raise Exception("Empty input string")
         i = 0
         while i < len(s):
             ch = s[i]
             if ch.isdigit():
                 self.tokens.append(('DIGIT', ch))
             elif ch == '+':
-                self.tokens.append(('PLUS', ch))
+                self.tokens.append(Lexer.PLUS)
             elif ch == '-':
-                self.tokens.append(('SUB', ch))
+                self.tokens.append(Lexer.SUB)
+            elif ch == '*':
+                self.tokens.append(Lexer.MUL)
+            elif ch == '/':
+                self.tokens.append(Lexer.DIV)
+            elif ch == '(':
+                self.tokens.append(Lexer.LPAREN)
+            elif ch == ')':
+                self.tokens.append(Lexer.RPAREN)
             elif ch.isspace():
                 # Is it is just space, like tabs or spaces just ignore
                 i += 1
@@ -39,7 +59,7 @@ class Lexer:
             i += 1
             
         # Adds the end of file token
-        self.tokens.append(('EOF', 'EOF'))
+        self.tokens.append(Lexer.EOF)
 
     def get_tokens(self):
         """ Return the list of tokens """
@@ -65,56 +85,89 @@ class Parser:
             self.current_token = self.tokens[self.position]
             self.position += 1
         else:
-            self.current_token = ('EOF', 'EOF')
+            self.current_token = Lexer.EOF
+            
+    def has_tokens(self):
+        """ Detects if there are more tokens """
+        return self.position < len(self.tokens)
+
+    def match(self, token : (str, str)) -> bool:
+        """ Matches the current token with a given """
+        return self.current_token[0] == token[0] and self.current_token[1] == token[1]
+
 
     def parse(self):
         """
         The main function that starts the syntax analizer
         """
-        self.parse_List() # First rule, of the grammar
+        self.expr() # First rule, of the grammar
         
         if self.current_token[0] != 'EOF':
-            raise SyntaxError("Stream of chars not valid for the grammar.")
+            raise SyntaxError(f"Expect EOF, but found '{self.current_token[1]}'.")
 
-    def parse_List(self):
+    def expr(self):
         """
-        List -> digit RestOfList
+        expr -> term
+              | term + term
+              | term - term
         """
-        self.parse_digit()     # First parse the digit
-        self.parse_RestOfList() # Then parse the rest of the list
+        self.term()     # First parse the digit
+        while self.match(Lexer.PLUS) or self.match(Lexer.SUB):
+            # Do something here ...
+            
+            self.next_token()
+            self.term()
 
-    def parse_RestOfList(self):
-        """
-        RestOfList -> + digit RestOfList
-                    | - digit RestOfList
-                    | e
-        """
-        # Since we have a recursive production here
-        # We need to fetch the plus token until get a different token
-        while self.current_token[0] == 'PLUS' or self.current_token[0] == 'SUB':
-            self.next_token()      # fetch '+' or '-'
-            self.parse_digit()    # parse a digit
-        # If there isn't another (+) token then it implies that there is lambda token (e)
 
-    def parse_digit(self):
+    def term(self):
         """
-        digit -> 0 | 1 | 2 | ... | 9
+        term -> factor
+              | factor * factor
+              | factor / factor
         """
-        if self.current_token[0] == 'DIGIT':
-            self.next_token()  # Parse a digit
+        self.factor()
+
+        while self.match(Lexer.MUL) or self.match(Lexer.DIV):
+            # Do something here ...
+            
+            self.next_token()
+            self.factor()
+            
+            
+
+
+    def factor(self):
+        """
+        factor -> digit
+                | ( expr )
+        """
+        if self.match(Lexer.LPAREN):
+            self.next_token()
+            self.expr()
+            if not self.match(Lexer.RPAREN):
+                raise SyntaxError(f"Expect to have ')' after having '(', but found {self.current_token[1]}")
+            self.next_token()
+        elif self.current_token[0] == 'DIGIT':
+            self.next_token()
         else:
-            raise SyntaxError(f"We expect a digit but it found: {self.current_token}")
+            raise SyntaxError(f"Expect to have a digit '0, 1, 2, ..., 9' or '(' with a new expression, but found '{self.current_token[1]}'")
+
+            
+
+            
 
 
 def main():
     # A usage example
     
-    test_string = "2+8-5-2"
+    # test_string = "  2 * ( 2 / ( 1 + 1))"
+    test_string = " 2 + 2 * (2 + 2) "
     
     try:
         # Get the stream of tokens
         lexer = Lexer(test_string)
         tokens = lexer.get_tokens()
+        print(tokens)
 
         # Parse the tokens
         parser = Parser(tokens)
@@ -127,6 +180,7 @@ def main():
     except ValueError as e:
         print("Value Error:", e)
 
+        
 if __name__ == "__main__":
     main()
     
